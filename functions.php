640c924fd875b482c566db524c87b8fffa422dfa
micskeil <micskeil@gmail.com>
2020-11-07 16:48:03 +0100

    enqueue scripts

--- functions.php
+++ functions.php
@@ -1,9 +1,17 @@
 <?php 
 
-function mokuslak_register_styles(){
-    wp_enqueue_style('mokuslak-style', get_template_directory_uri() . '/style.css' , array(), '1.0', 'all');
-}
+function mokuslak_register_styles() {
+    wp_enqueue_style('mokuslak-bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css' , array(), '4.3.0', 'all');
+    wp_enqueue_style('mokuslak-style', get_template_directory_uri() . '/style.css' , array('mokuslak-bootstrap'), '1.0', 'all');
+    wp_enqueue_style('mokuslak-fonts', 'https://fonts.googleapis.com/css?family=PT+Serif|Rufina:700' , array(), '1.0', 'all');
+    
+    wp_enqueue_script('mokuslak-jquery','https://code.jquery.com/jquery-3.3.1.js', array(), $ver = false, $in_footer = true);
+    wp_enqueue_script('mokuslak-flip','https://cdn.rawgit.com/nnattawat/flip/master/dist/jquery.flip.min.js', array(), '1.0', false);
+    wp_enqueue_script('mokuslak-bootstrap','https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/js/bootstrap.min.js', array(), '1.0', 'all');
+    wp_enqueue_script('mokuslak-recaptcha','https://www.google.com/recaptcha/api.js?render=reCAPTCHA_site_key', array(), '1.0', 'all');
+    
 
+}
 
 add_action( 'wp_enqueue_scripts', 'mokuslak_register_styles');
 
--- sections/head.php
+++ sections/head.php
@@ -10,16 +10,7 @@ $homepage = false;
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <script
-  src="https://code.jquery.com/jquery-3.3.1.js"
-  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
-  crossorigin="anonymous" lazyload></script>
-  <script src="https://cdn.rawgit.com/nnattawat/flip/master/dist/jquery.flip.min.js"></script>
-    <!-- Bootstrap -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous" lazyload></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css" integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA" crossorigin="anonymous" lazyload>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/js/bootstrap.min.js" integrity="sha384-7aThvCh9TypR7fIc2HV4O/nFMVCBwyIUKL8XCtKE+8xgCgl/PQGuFsvShjr74PBp" crossorigin="anonymous" lazyload></script>
-    <script src="https://www.google.com/recaptcha/api.js?render=reCAPTCHA_site_key"></script>
+    
   <script>
   grecaptcha.ready(function() {
       grecaptcha.execute('reCAPTCHA_site_key', {action: 'homepage'}).then(function(token) {
@@ -27,16 +18,8 @@ $homepage = false;
       });
   });
   </script>
-  
-<link href="https://fonts.googleapis.com/css?family=PT+Serif|Rufina:700" rel="stylesheet">
 
-    <!-- Custom styles 
-
-    <link rel="stylesheet" href="<?php echo get_theme_file_uri( 'style.css' ); ?>"> 
-
-    ez csak, ha nem működne a function php stylesheet beszedése
-  
-    -->
+    <!-- Custom, bootstrap styles and costum fonts -->
 
     <?php 
     wp_head();
